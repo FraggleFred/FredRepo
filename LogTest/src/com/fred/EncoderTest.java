957143e41232c0229c44021410b4cfffa0d54d77
Fred <rainer.murawsk@gmail.com>
2019-04-26 10:56:47 +0200

    Initial

---
+++ LogTest/src/com/fred/EncoderTest.java
@@ -0,0 +1,52 @@
+
+package com.fred;
+
+import org.owasp.encoder.Encode;
+
+
+public class EncoderTest
+{
+  /**
+   * Default constructor
+   */
+  public EncoderTest()
+  {
+  }
+
+
+  /**
+   * Test method
+   * 
+   * @param pArgs
+   *          Command line arguments
+   */
+  @SuppressWarnings("deprecation")
+  public static void main(String[] pArgs)
+  {
+    try
+    {
+      String xtUrl = "https://adpworld.staging.ehc.adp.com/xtimewf/gwmenu-war?LOGIN=VVNFUklEPWRlODQ3MzY0JkRPTUFJTj1kZS5iZTEmVElNRVNUQU1QPTE4MDMyMDE5MTA1MTU4JlZB&#10;TFVFPTc4MzExNTA2Q0I1RDZENTI3MTY0QkExMDQxQzEwMkND";
+      String instId = "vmosbingsa:/appl/prod/ADVT14:ADVT14";
+      String iHCMUrl = "https://www.adpworld.de/whrm";
+      String jsUrl = "<script>alert(1);</script>";
+
+      System.out.println(iHCMUrl);
+      System.out.println(Encode.forUri(iHCMUrl));
+      System.out.println(Encode.forUri(jsUrl));
+      System.out.println(Encode.forUriComponent(iHCMUrl));
+
+      System.out.println();
+      System.out.println(instId);
+      System.out.println(Encode.forHtml(instId));
+
+      System.out.println();
+      System.out.println(xtUrl);
+      System.out.println(Encode.forUri(xtUrl));
+      System.out.println(Encode.forUriComponent(xtUrl));
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ LogTest/src/com/fred/LoggingTest.java
@@ -0,0 +1,63 @@
+
+package com.fred;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * Log4j2 test class
+ */
+public class LoggingTest
+{
+  // member
+  private static final Logger logM = LogManager.getFormatterLogger(LoggingTest.class);
+
+
+  /**
+   * Default constructor
+   */
+  public LoggingTest()
+  {
+  }
+
+
+  /**
+   * Do some logging
+   */
+  public void logit()
+  {
+    for (int i = 0; i < 3000; i++)
+    {
+      if (logM.isInfoEnabled())
+      {
+        logM.info("logit() info " + i);
+      }
+
+      if (logM.isErrorEnabled())
+      {
+        logM.error("logit() error " + i);
+      }
+    }
+  }
+
+
+  /**
+   * Test method
+   * 
+   * @param args
+   *          Command line arguments
+   */
+  public static void main(String[] args)
+  {
+    try
+    {
+      LoggingTest lt = new LoggingTest();
+      lt.logit();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
