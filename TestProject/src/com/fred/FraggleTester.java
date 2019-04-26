4eb1d7be9054094ecf97fe9a06dacdcf0fa85491
Fred <rainer.murawsk@gmail.com>
2019-04-26 13:50:35 +0200

    pull it

--- TestProject/src/com/fred/FraggleTester.java
+++ TestProject/src/com/fred/FraggleTester.java
@@ -7,7 +7,7 @@ public class FraggleTester
   {
     try
     {
-      System.out.print("FraggleTester() entered again");
+      System.out.print("FraggleTester() entered again and again");
     }
     catch (Exception e)
     {
